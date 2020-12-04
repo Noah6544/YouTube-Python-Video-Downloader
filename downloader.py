a5979c14a5215633350bdfe2640849bf8d8e0568
Noah <56573280+Noah6544@users.noreply.github.com>
2020-12-04 17:44:59 -0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -3,25 +3,6 @@ import time
 import random
 
 
-def timestuff():
-    randomnum = random.randint(3, 9)
-    for i in range(0, randomnum):
-        randomnum = random.randint(1, 9)
-        if randomnum == 1:
-            print("PLEASE WAIT...")
-        elif randomnum == 2:
-            print("WORKING...")
-        elif randomnum == 3:
-            print("ALMOST THERE...")
-        elif randomnum == 4:
-            print("PREPARING DOWNLOAD...")
-        # elif random > 4 and <= 7:
-        # print("WAIT WHILE WE SET IT UP FOR YOU...")
-        elif randomnum == 8:
-            print("NEARLY!")
-        elif randomnum == 9:
-            print("IDK WAIT")
-
 
 def download():
     print("\n")
@@ -34,17 +15,16 @@ def download():
             yt = YouTube(link)
             correct = input("\nTHE TITLE OF THIS VIDEO IS: \" " + str(yt.title) + "\"  IS THIS CORRECT? (y/n):\n ")
             if correct == "y":
-                yt.streams.filter(progressive=True).last().download(dir)
                 print("\nDOWNLOADING...")
+                yt.streams.filter(progressive=True).last().download(dir)                
                 print("\nDOWNLOAD COMPLETE\n")
                 YES = input("DO YOU WANT TO DOWNLOAD ANOTHER VIDEO? (y/n): \n")
                 if YES == "y":
                     continue
                 elif YES == "n":
-                    print("\nTHANKS FOR USING THIS PROGRAM, WRITTEN BY NOAH(not pytube)")
                     break
                 else:
-                    print("OOPS ERROR RUN AGAIN\n")
+                    print("AN ERROR OCCURED, RUN AGAIN\n")
             elif correct == "n":
                 continue
             else:
